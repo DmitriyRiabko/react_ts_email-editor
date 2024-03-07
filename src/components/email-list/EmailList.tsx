ce0b5d17d03231c99db032e6fe1bc1e276284c19
vsegda_vinovat <riabko97@gmail.com>
2024-03-07 19:13:05 +0200

    added list of emails from db

--- src/components/email-list/EmailList.tsx
+++ src/components/email-list/EmailList.tsx
@@ -1,7 +1,17 @@
+import { useQuery } from '@tanstack/react-query'
 import styles from './EmailList.module.scss'
-
+import { emailService } from '../../services/email.services'
+import HTMLReactParser from 'html-react-parser/lib/index'
 
 export function EmailList() {
-    return <div className={styles.list}>
-        EmailList</div>
+    const { data } = useQuery({
+        queryKey: ['email list'],
+        queryFn: () => emailService.getEmails()
+    })
+
+    return (<div className={styles.list}>
+        {data?.map(email => (
+            <div key={email.text}>{ HTMLReactParser(email?.text)}</div>
+        ))}
+    </div>)
 }
